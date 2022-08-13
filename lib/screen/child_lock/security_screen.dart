1830585d8e43c38a1a0dfd4287e2636d9ef4e87a
talha828 <talhaiqbal246@gmail.com>
2022-08-13 14:28:25 +0500

    last work

--- lib/screen/child_lock/security_screen.dart
+++ lib/screen/child_lock/security_screen.dart
@@ -30,11 +30,98 @@ class _SecurityScreenState extends State<SecurityScreen> {
   var date = DateTime.now();
   String code;
   String dateCheck;
-
+  // void getAllCustomerData(bool data) async {
+  //   if(true){
+  //     try {
+  //       String actualAddress;
+  //       Coordinates userLatLng;
+  //       List<CustomerModel>customer;
+  //       Provider.of<CustomerList>(context,listen: false).setLoading(true);
+  //       var data =await loc.Location().getLocation();
+  //       List<AddressModel>addressList=[];
+  //       userLatLng=Coordinates(data.latitude,data.longitude);
+  //       String mapApiKey="AIzaSyDhBNajNSwNA-38zP7HLAChc-E0TCq7jFI";
+  //       String _host = 'https://maps.google.com/maps/api/geocode/json';
+  //       final url = '$_host?key=$mapApiKey&language=en&latlng=${userLatLng.latitude},${userLatLng.longitude}';
+  //       print(url);
+  //       if(userLatLng.latitude != null && userLatLng.longitude != null){
+  //         var response1 = await http.get(Uri.parse(url));
+  //         if(response1.statusCode == 200) {
+  //           Map data = jsonDecode(response1.body);
+  //           String _formattedAddress = data["results"][0]["formatted_address"];
+  //           var address = data["results"][0]["address_components"];
+  //           for(var i in address){
+  //             addressList.add(AddressModel.fromJson(i));
+  //           }
+  //           actualAddress=addressList[3].shortName;
+  //           Provider.of<CustomerList>(context,listen: false).updateAddress(actualAddress);
+  //           print("response ==== $_formattedAddress");
+  //           _formattedAddress;
+  //         }
+  //         var response = await OnlineDatabase.getAllCustomer();
+  //         print("Response code is " + response.statusCode.toString());
+  //         if (response.statusCode == 200) {
+  //           var data = jsonDecode(utf8.decode(response.bodyBytes));
+  //           //print("Response is" + data.toString());
+  //
+  //           for (var item in data["results"]) {
+  //             double dist=calculateDistance(double.parse(item["LATITUDE"].toString()=="null"?1.toString():item["LATITUDE"].toString()), double.parse(item["LONGITUDE"].toString()=="null"?1.toString():item["LONGITUDE"].toString()),userLatLng.latitude,userLatLng.longitude);
+  //             customer.add(CustomerModel.fromModel(item,distance: dist));
+  //           }
+  //
+  //           for(int i=0; i < customer.length-1; i++){
+  //             for(int j=0; j < customer.length-i-1; j++){
+  //               if(customer[j].distance > customer[j+1].distance){
+  //                 CustomerModel temp = customer[j];
+  //                 customer[j] = customer[j+1];
+  //                 customer[j+1] = temp;
+  //               }
+  //             }
+  //           }
+  //           Provider.of<CustomerList>(context,listen: false).clearList();
+  //           Provider.of<CustomerList>(context,listen: false).storeResponse(data);
+  //           Provider.of<CustomerList>(context,listen: false).getAllCustomer(customer);
+  //           Provider.of<CustomerList>(context,listen: false).getDues(customer);
+  //           Provider.of<CustomerList>(context,listen: false).getAssignShop(customer);
+  //           print("done");
+  //           setState(() {
+  //
+  //           });
+  //           //print("length is"+limitedcustomer.length.toString());
+  //           Provider.of<CustomerList>(context,listen: false).setLoading(false);
+  //
+  //         } else if (response.statusCode == 400) {
+  //           var data = jsonDecode(utf8.decode(response.bodyBytes));
+  //           Fluttertoast.showToast(
+  //               msg: "${data['results'].toString()}",
+  //               toastLength: Toast.LENGTH_SHORT,
+  //               backgroundColor: Colors.black87,
+  //               textColor: Colors.white,
+  //               fontSize: 16.0);
+  //           Provider.of<CustomerList>(context,listen: false).setLoading(false);
+  //         }}
+  //     } catch (e, stack) {
+  //       print('exception is' + e.toString());
+  //       Fluttertoast.showToast(
+  //           msg: "Error: " + e.toString(),
+  //           toastLength: Toast.LENGTH_SHORT,
+  //           backgroundColor: Colors.black87,
+  //           textColor: Colors.white,
+  //           fontSize: 16.0);
+  //       Provider.of<CustomerList>(context,listen: false).setLoading(false);
+  //     }
+  //   }
+  // }
+  // calculateDistance(double lat1,double long1,double lat2,double long2) {
+  //   var distance = geo.Geolocator.distanceBetween(lat2,
+  //       long2, lat1, long1);
+  //   return distance / 1000;
+  // }
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    //getAllCustomerData(true);
     txt1=TextEditingController();
     txt2=TextEditingController();
     txt3=TextEditingController();
