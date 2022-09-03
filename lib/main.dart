37d2e24b23e763ef8c9cf54ae82f608630e628a2
talha828 <talhaiqbal246@gmail.com>
2022-09-03 18:15:01 +0500

    andriod

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ void main() async{
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<UserModel>(
